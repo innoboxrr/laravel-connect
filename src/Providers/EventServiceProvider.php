bf61a6894f9cbdb747d41bd7a369b8933a2418d5
Homero Raul <h.raulvc@gmail.com>
2025-04-22 10:08:57 -0600

    Update EventServiceProvider.php

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -15,7 +15,7 @@ class EventServiceProvider extends ServiceProvider
 
     private function registerEventsAndObservers()
     {
-        $cacheKey = 'events_and_observers';
+        $cacheKey = 'laravel_connect_events_and_observers';
 
         $data = Cache::remember($cacheKey, now()->addDay(), function () {
             return [
